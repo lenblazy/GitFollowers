3fc9469eee9d6ef870294e01217ebd0565bcf45c
lenblazy <lenblazy@gmail.com>
2024-02-10 12:59:42 +0300

    Initial Commit

---
+++ GitFollowers/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GitFollowers
+//
+//  Created by Lennox Mwabonje on 10/02/2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
