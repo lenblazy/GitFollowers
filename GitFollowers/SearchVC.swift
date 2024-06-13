bf709810f1545d285d51bec8fbcec34ccddf712b
lenblazy <lenblazy@gmail.com>
2024-06-13 10:10:40 +0300

    Add image to search view controller

--- GitFollowers/SearchVC.swift
+++ GitFollowers/SearchVC.swift
@@ -8,12 +8,35 @@
 import UIKit
 
 class SearchVC: UIViewController {
+    
+    let logoImageView   = UIImageView()
+    let tfUserName      = GFTextField()
+    let btnCallToAction = GFButton(backgroundColor: .systemGreen, title: "Get Followers")
 
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        view.backgroundColor = .systemRed
+        view.backgroundColor = .systemBackground
+        configureLogoImageView()
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        navigationController?.isNavigationBarHidden = true
     }
 
+    private func configureLogoImageView() {
+        view.addSubview(logoImageView)
+        logoImageView.translatesAutoresizingMaskIntoConstraints = false
+        logoImageView.image = UIImage(named: "gh-logo")!
+        
+        NSLayoutConstraint.activate([
+            logoImageView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 80),
+            logoImageView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            logoImageView.heightAnchor.constraint(equalToConstant: 200),
+            logoImageView.widthAnchor.constraint(equalToConstant: 200)
+        ])
+        
+    }
 
 }
