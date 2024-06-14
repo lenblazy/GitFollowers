710fad3b4016d9cf24167cd9f2d0f23296dced41
lenblazy <lenblazy@gmail.com>
2024-06-14 09:02:31 +0300

    Add network models

---
+++ GitFollowers/Model/Follower.swift
@@ -0,0 +1,13 @@
+//
+//  Follower.swift
+//  GitFollowers
+//
+//  Created by Lennox Mwabonje on 14/06/2024.
+//
+
+import Foundation
+
+struct Follower: Codable {
+    let login: String
+    let avatarUrl: String
+}
---
+++ GitFollowers/Model/User.swift
@@ -0,0 +1,22 @@
+//
+//  User.swift
+//  GitFollowers
+//
+//  Created by Lennox Mwabonje on 14/06/2024.
+//
+
+import Foundation
+
+struct User: Codable {
+    let login: String
+    let avatarUrl: String
+    let name: String?
+    let location: String?
+    let bio: String?
+    let publicRepos: Int
+    let publicGists: Int
+    let htmlUrl: String
+    let following: Int
+    let followers: Int
+    var createdAt: String
+}
