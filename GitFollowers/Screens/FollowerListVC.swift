8f012770d86f5acfdcc6baf6411c423a1bfe7cc2
lenblazy <lenblazy@gmail.com>
2024-06-15 13:18:50 +0300

    Add loading state

--- GitFollowers/Screens/FollowerListVC.swift
+++ GitFollowers/Screens/FollowerListVC.swift
@@ -61,8 +61,10 @@ class FollowerListVC: UIViewController {
     }
     
     private func getFollowers(username: String, page: Int) {
+        showLoadingView()
         NetworkManager.shared.getFollowers(for: userName, page: page) { [weak self] result in
             guard let self = self else { return }
+            self.dismissLoadingView()
             
             switch (result){
             case .success(let followers):
