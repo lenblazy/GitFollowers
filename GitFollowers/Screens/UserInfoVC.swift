c12f050f5ba04013e5d06cc504a9bff6c4efa403
lenblazy <lenblazy@gmail.com>
2024-06-15 17:38:40 +0300

    Add header view controller

--- GitFollowers/Screens/UserInfoVC.swift
+++ GitFollowers/Screens/UserInfoVC.swift
@@ -9,6 +9,8 @@ import UIKit
 
 class UserInfoVC: UIViewController {
     
+    let headerView = UIView()
+    
     var username: String!
 
     override func viewDidLoad() {
@@ -16,11 +18,43 @@ class UserInfoVC: UIViewController {
         view.backgroundColor    = .systemBackground
         let doneButton = UIBarButtonItem(barButtonSystemItem: .done, target: self, action: #selector(dismissVC))
         navigationItem.rightBarButtonItem = doneButton
+        layoutViews()
+        
+        NetworkManager.shared.getUserInfo(for: username) { [weak self] result in
+            guard let self = self else { return }
+            
+            switch result{
+            case .success(let user): DispatchQueue.main.async {
+                self.add(childVc: GFUserInfoHeaderVC(user: user), to: self.headerView)
+            }
+            case .failure(let error): self.presentGFAlert(title: "Something went wrong", message: error.rawValue, buttonText: "Ok")
+            }
+        }
+        
     }
     
     @objc func dismissVC(){
         dismiss(animated: true)
     }
     
+    private func layoutViews(){
+        view.addSubview(headerView)
+        headerView.translatesAutoresizingMaskIntoConstraints = false
+        
+        NSLayoutConstraint.activate([
+            headerView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            headerView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            headerView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            headerView.heightAnchor.constraint(equalToConstant: 180)
+        ])
+        
+    }
+    
+    private func add(childVc: UIViewController, to containerView: UIView){
+        addChild(childVc)
+        containerView.addSubview(childVc.view)
+        childVc.view.frame = containerView.bounds
+        childVc.didMove(toParent: self)
+    }
 
 }
---
+++ GitFollowers/Utilities/Constants.swift
@@ -0,0 +1,12 @@
+//
+//  Constants.swift
+//  GitFollowers
+//
+//  Created by Lennox Mwabonje on 15/06/2024.
+//
+
+import Foundation
+
+enum SFSymbols {
+    static let location = "mappin.and.ellipse"
+}
