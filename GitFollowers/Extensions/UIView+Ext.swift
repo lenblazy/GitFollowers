a8b7461552aa7d2ee9724f56d036631b0f1e89db
lenblazy <lenblazy@gmail.com>
2024-06-17 09:04:27 +0300

    use variadic parametwers to add subviews

---
+++ GitFollowers/Extensions/UIView+Ext.swift
@@ -0,0 +1,17 @@
+//
+//  UIView+Ext.swift
+//  GitFollowers
+//
+//  Created by Lennox Mwabonje on 17/06/2024.
+//
+
+import UIKit
+
+extension UIView {
+    
+    func addSubViews(_ views: UIView...){
+        for view in views { addSubview(view) }
+    }
+    
+    
+}
